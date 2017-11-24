ad0a85b458ab53cb3b8ece550a0f2ef5ebfbcce2
Anna Lotko <annaklotko@gmail.com>
2017-11-24 10:10:38 -0700

    added grouping in progress

--- src/js/components/pages/Canvas.js
+++ src/js/components/pages/Canvas.js
@@ -10,6 +10,7 @@ export default class Canvas extends React.Component {
     this.positionData = [
       {
         id:"background",
+        children: [],
         scaleX: .5,
         scaleY: .5,
         x:0,
@@ -20,13 +21,36 @@ export default class Canvas extends React.Component {
       },
       {
         id:"melinda_full",
+        children: [
+          {
+            id: "melinda_eye",
+            img: {src:"/assets/melinda_eye_closed.png"},
+            scaleX: .5,
+            scaleY: .5,
+            x: this.width - 500,
+            y: this.height,
+            steps: []
+          }
+        ],
         scaleX: .75,
         scaleY: .75,
-        x:this.width / 1.5,
-        y:this.height - 50,
+        anchorX: 0,
+        anchorY: 0,
+        x:this.width - 600,
+        y:this.height,
         rot: 0,
         img: {},
         steps: [
+          {
+            wait: 1000,
+            x: 0,
+            y: 0,
+            t: 0,
+            sx: .75,
+            sy: .75,
+            easePow: 0,
+            rot: 0
+          },
           {
             wait: 500,
             x: -50,
@@ -38,9 +62,39 @@ export default class Canvas extends React.Component {
             rot: 0
           },
           {
-            wait: 5000,
+            wait: 3000,
             x: 0,
             y: 250,
+            t: 1000,
+            sx: .75,
+            sy: .75,
+            easePow: 1,
+            rot: 0
+          },
+          {
+            wait: 0,
+            x: -100,
+            y: 0,
+            t: 0,
+            sx: .75,
+            sy: .75,
+            easePow: 1,
+            rot: 0
+          },
+          {
+            wait: 1000,
+            x: 0,
+            y: -800,
+            t: 1000,
+            sx: .75,
+            sy: .75,
+            easePow: 1,
+            rot: 0
+          },
+          {
+            wait: 1000,
+            x: -150,
+            y: 0,
             t: 1000,
             sx: .75,
             sy: .75,
@@ -51,34 +105,57 @@ export default class Canvas extends React.Component {
       },
       {
         id:"melinda_hand",
+        children: [],
         scaleX: .35,
         scaleY: .35,
-        x:(this.width / 1.5) - 100,
+        anchorX: 2,
+        anchorY: 1.5,
+        x:this.width - 275,
         y:this.height,
         img: {},
         steps: [
           {
-            wait: 1000,
-            x: -50,
-            y: -100,
+            wait: 3000,
+            x: 0,
+            y: 0,
+            t: 0,
+            sx: .35,
+            sy: .35,
+            easePow: 0,
+            rot: 0
+          },
+          {
+            wait: 100,
+            x: 0,
+            y: 0,
+            t: 500,
+            sx: .35,
+            sy: .35,
+            easePow: 3,
+            rot: -25
+          },
+          {
+            wait: 300,
+            x: 0,
+            y: 0,
+            t: 500,
+            sx: .35,
+            sy: .35,
+            easePow: 3,
+            rot: 25
+          },
+          {
+            wait: 300,
+            x: 50,
+            y: 150,
             t: 1000,
             sx: .35,
             sy: .35,
             easePow: 3,
             rot: 0
           },
-          {
-            wait: 1000,
-            x: 0,
-            y: 0,
-            t: 1000,
-            sx: .35,
-            sy: .35,
-            easePow: 3,
-            rot: -35
-          }
         ]
-      },
+      }
     ]
 
     this.handleFileComplete = this.handleFileComplete.bind(this);
@@ -98,12 +175,12 @@ export default class Canvas extends React.Component {
     queue.loadFile({id:"background", src:"/assets/background-kitchen-ipadhd.png"});
     queue.loadFile({id:"melinda_full", src:"/assets/melinda-c1p1-ipadhd.png"});
     queue.loadFile({id:"melinda_hand", src:"/assets/melinda_hand.png"});
+    // queue.loadFile({id:"melinda_eye", src:"/assets/melinda_eye_closed.png"});
     queue.load();
   }
 
   handleFileComplete(e) {
     let img = new createjs.Bitmap(e.item.src);
-    img = this.setInitPositionData(img, e.item.id)
     this.setImage(img, e.item.id);
   }
 
@@ -111,13 +188,50 @@ export default class Canvas extends React.Component {
     let data = this.positionData.find((p) => {
       return p.id === id;
     })
+    if (data.children.length > 0) {
+      this.setImageGroup(img, data);
+      return;
+    }
+    img = this.setInitPositionData(img, id)
     this.renderCanvas();
     // push loaded image to data set
     data.img = img;
     this.setSteps(img, data, 0);
+
     // Add starting assets to the stage
     this.stage.addChild(img);
+  }
 
+  // if there are children, create a group
+  setImageGroup(img, data) {
+    var container = new createjs.Container();
+    container.backgroundColor = "#000"
+    container.addChild(img);
+    var queue = new createjs.LoadQueue(true);
+    queue.on("complete", this.positionGroup.bind(this, container, data));
+    for (var i = 0; i < data.children.length; i++) {
+      queue.on("fileload", this.handleChildLoad.bind(this, container, data.children[i]));
+      queue.loadFile({
+        id:data.children[i].id,
+        src:data.children[i].img["src"]
+      });
+    }
+  }
+
+  handleChildLoad(container, subData, e){
+    let img = new createjs.Bitmap(e.item.src);
+    // TODO find a better way of setting children objects
+    img.x = subData.x;
+    img.y = subData.y;
+    img.scaleX = subData.scaleX;
+    img.scaleY = subData.scaleY;
+    container.addChild(img);
+  }
+
+  positionGroup(container, data, e) {
+    this.setSteps(container, data, 0);
+    // Add starting assets to the stage
+    this.stage.addChild(container);
   }
 
   renderCanvas() {
@@ -125,7 +239,7 @@ export default class Canvas extends React.Component {
   }
 
   setSteps(obj, data, i, startingX, startingY) {
-    this.recenter(obj);
+    this.recenter(obj, data);
     if (i === data.steps.length) {
       return;
     }
@@ -156,16 +270,15 @@ export default class Canvas extends React.Component {
     img.y = data.y
     img.scaleX = data.scaleX;
     img.scaleY = data.scaleY;
-    this.recenter(img);
+    this.recenter(img, data);
     return img;
   }
 
-  recenter(obj) {
+  recenter(obj, data) {
   	var bds = obj.getBounds();
     if (bds === null) return;
-  	obj.regX = bds.width/2;
-  	obj.regY = bds.height/2;
-    console.log(obj.regX, obj.regY);
+    obj.regX = data.anchorX === 0 ? 0: bds.width/data.anchorX;
+    obj.regY = data.anchorY === 0 ? 0: bds.height/data.anchorY;
   }
 
   render() {
